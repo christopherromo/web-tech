debdb6d78d8300a2333e75c9ca96d585ef11c090
Christopher Romo <cromo@uccs.edu>
2026-06-03 01:22:15 -0600

    pokemon league api commit 1
    - initialized mini-project
    - finished database.js

---
+++ sql/pokemon league api mini-project/src/server.js
@@ -0,0 +1,29 @@
+/**
+ * server.js
+ *
+ * the entry point for the pokemon league api server.
+ *
+ * author: christopher romo
+ * created: 2026-06-03
+ */
+
+import "dotenv/config";
+import express from "express";
+
+// set the port and create the express app
+const PORT = 3000;
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.static("src/public"));
+
+// handle 404 errors for undefined routes
+app.use((req, res) => {
+  res.status(404).json({
+    message: "endpoint not found.",
+  });
+});
+
+// listen on the specified port
+app.listen(PORT, () => console.log("server running at http://localhost:3000"));
